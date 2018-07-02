2edc20d8b8df2f5b2ab57fda3024e0c8263c6105
Unknown <aiyuixps13@outlook.jp>
2018-07-02 18:07:32 +0900

    コミット

---
+++ Igaguri/Assets/chapter7/IgaguriGeneraor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgaguriGeneraor : MonoBehaviour {
+
+    public GameObject igaguriPrefab;
+    public int slowdown = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject igaguri = Instantiate(igaguriPrefab) as GameObject;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 worldDir = ray.direction;
+            igaguri.GetComponent<IgaguriController>().shoot(worldDir.normalized * 2000 * slowdown);
+            //if (slowdown > 0)
+            //{
+            //    slowdown -= 200;
+            //}
+        }
+	}
+}
